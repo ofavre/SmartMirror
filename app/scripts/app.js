0840e8571efddc370aca730464c31e5f8fd4936f
Olivier Favre <of.olivier.favre@gmail.com>
2016-03-04 00:09:47 +0100

    Show article in a dialog box using Readability

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,4 +14,48 @@
     { url: "http://www.lemonde.fr/sciences/rss_full.xml", alias: "Sciences - Le Monde" },
   ];
 
+  app.readableify = function(url) {
+    return new Promise(function(resolve, reject) {
+      jQuery.ajax({
+        method: 'GET',
+        url: "https://cors-anywhere.herokuapp.com/http://www.readability.com/api/content/v1/parser",
+        data: {
+          url: url,
+          token: 'c63bfc7cd66292cfd16caa30cd8eae0eafa42f47',
+        }
+      }).done(function(data, textStatus, xhr) {
+        resolve(data);
+      }).fail(function(xhr, textStatus, error) {
+        console.error('Failed to readableify url', url, ':', error);
+        resolve(url);
+      });
+    });
+  };
+
+  app.onClickOpenReadableLink = function(event) {
+    event.preventDefault();
+    var link = event.path.filter(e => e.tagName == 'A')[0];
+    var url = link.getAttribute('href');
+    var toast = document.createElement('paper-toast');
+    document.body.appendChild(toast);
+    toast.show({ text: 'Opening link…', duration: Infinity });
+    app.readableify(url).then(function(dataOrUrl) {
+      toast.toggle();
+      if (typeof dataOrUrl == 'string') {
+        window.location = url;
+      } else {
+        var data = dataOrUrl;
+        console.log(data);
+        var dialog = document.createElement('paper-dialog');
+        // TODO: use document.createElement('paper-dialog-scrollable')
+        dialog.innerHTML = '<h2>' + data.title + '</h2><div><paper-dialog-scrollable>' + data.content + '</paper-dialog-scrollable></div><div class="buttons"><paper-button dialog-confirm autofocus>OK</paper-button></div>';
+        dialog.withBackdrop = true;
+        dialog.entryAnimation = 'scale-up-animation';
+        dialog.exitAnimation = 'scale-down-animation';
+        document.body.appendChild(dialog);
+        dialog.toggle();
+      }
+    });
+  };
+
 })(document);
