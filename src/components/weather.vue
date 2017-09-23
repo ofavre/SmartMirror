447e30ae243eb02ceacbfe6c7d9ffa9f30e30bd2
Olivier Favre <olivier@wonderpush.com>
2017-09-23 22:36:32 +0200

    Drop charted info, smaller and graph aligned list

--- src/components/weather.vue
+++ src/components/weather.vue
@@ -1,25 +1,24 @@
 <template>
   <div>
-    <svg style="display:none;">
-      <defs>
-        <path id="drop-filled"
-          d="M413.289,200.718c-0.016-0.021-0.03-0.041-0.045-0.061C407.69,193.171,277.11,17.178,267.635,5.534
-                  c-2.918-3.587-7.379-5.634-11.949-5.53c-4.623,0.097-8.942,2.32-11.708,6.026L100.036,198.936
-                  C74.498,232.909,61,273.39,61,316.001c0,108.075,87.477,196,195,196s195-87.925,195-196
-                  C451,274.189,437.96,234.324,413.289,200.718z"
-        />
-        <path
-          id="drop-contour"
-          d="M413.289,200.718c-0.016-0.021-0.03-0.041-0.045-0.061C407.69,193.171,277.11,17.178,267.635,5.534
-                  c-2.918-3.587-7.379-5.634-11.949-5.53c-4.623,0.097-8.942,2.32-11.708,6.026L100.036,198.936
-                  C74.498,232.909,61,273.39,61,316.001c0,108.075,87.477,196,195,196s195-87.925,195-196
-                  C451,274.189,437.96,234.324,413.289,200.718z
-                  M256,482.001c-90.981,0-165-74.467-165-166c0-36.057,11.417-70.304,33.049-99.081
-                  l132.08-177.01c27.492,36.534,94.768,127.061,133.009,178.605C409.984,246.929,421,280.637,421,316.001
-                  C421,407.534,346.981,482.001,256,482.001z"
-        />
-      </defs>
-    </svg>
+    <ul class="weatherForecast" v-if="forecast !== null">
+      <li class="weatherForecastItem" v-for="(item, index) in forecast.hourly_forecast" :key="index">
+        <div class="weatherForecastItemInfo">
+          <div class="weatherForecastItemInfoTime">
+            {{item.FCTTIME.hour}}<small>h</small>
+          </div>
+        </div>
+        <div class="weatherForecastItemInfo">
+          <div class="weatherForecastItemInfoIcon">
+            <i :class="['wu', 'wu-white', 'wu-'+item.icon]"></i>
+          </div>
+        </div>
+        <div class="weatherForecastItemInfo">
+          <div class="weatherForecastItemInfoWind">
+            <img src="/static/winds-symbol.svg" :style="windStyle(item)"/>
+          </div>
+        </div>
+      </li>
+    </ul>
     <div class="chart-container chart-temp">
       <canvas id="chartTemp"></canvas>
     </div>
@@ -29,81 +28,6 @@
     <div class="chart-container chart-sky-air">
       <canvas id="chartSkyAir"></canvas>
     </div>
-    <ul class="weatherForecast" v-if="forecast !== null">
-      <li class="weatherForecastItem" v-for="(item, index) in forecast.hourly_forecast" :key="index">
-        <div class="weatherForecastItemInfo">
-          <div class="weatherForecastItemInfoTime">
-            {{item.FCTTIME.hour}}h
-          </div>
-        </div>
-        <!--div class="weatherForecastItemInfo">{{item.condition}}</div-->
-        <div class="weatherForecastItemInfo">
-          <div class="weatherForecastItemInfoIcon">
-            <i :class="['wu', 'wu-white', 'wu-64', 'wu-'+item.icon]"></i>
-          </div>
-        </div>
-        <div class="weatherForecastItemInfo">
-          <div class="weatherForecastItemInfoWind">
-            <img src="/static/winds-symbol.svg" :style="windStyle(item)"/>
-          </div>
-          {{item.wspd.metric}} <sup>km</sup>/<sub>h</sub>
-        </div>
-        <div class="weatherForecastItemInfo">
-          <div class="weatherForecastItemInfoRain">
-            <svg viewBox="0 0 512 512">
-              <defs>
-                <clipPath :id="`precipitations-${index}-down`">
-                  <rect x="0" :y="512 - rainScale(item) * 512" width="512" height="512"/>
-                </clipPath>
-                <clipPath :id="`precipitations-${index}-up`">
-                  <rect x="0" :y="0 - rainScale(item) * 512" width="512" height="512"/>
-                </clipPath>
-              </defs>
-              <use href="#drop-filled"
-                   fill="#3390FF"
-                   :clip-path="`url(#precipitations-${index}-down)`"
-              />
-              <use href="#drop-contour"
-                   :clip-path="`url(#precipitations-${index}-up)`"
-                   fill="#FFFFFF" fill-opacity="1"/>
-            </svg>
-          </div>
-          {{qpfMmLabel(item)}}<small>mm</small> {{item.pop}}%
-        </div>
-        <div class="weatherForecastItemInfo">
-          <div class="weatherForecastItemInfoTemperature">
-            <img src="/static/temperature.svg"/>
-            <template v-if="item.temp.metric === item.feelslike.metric">
-              {{item.temp.metric}}&deg;C
-            </template>
-            <template v-else>
-              {{item.feelslike.metric}}&deg;C <small>({{item.temp.metric}}&deg;C)</small>
-            </template>
-          </div>
-        </div>
-        <div class="weatherForecastItemInfo">
-          <div class="weatherForecastItemInfoCloudiness">
-            <img src="/static/cloud-filled.svg" :style="{opacity: parseFloat(item.sky)/100.}"/> {{item.sky}}%
-          </div>
-        </div>
-        <div class="weatherForecastItemInfo">
-          <div class="weatherForecastItemInfoUV">
-            <img src="/static/uv.svg"/> {{uvIndexLabel(item)}}
-          </div>
-        </div>
-        <div class="weatherForecastItemInfo">
-          <div class="weatherForecastItemInfoHumidity">
-            <img src="/static/humidity.svg"/> {{item.humidity}}%
-          </div>
-        </div>
-        <div class="weatherForecastItemInfo" v-if="item.snow.metric !== '0'">Snow: {{item.snow.metric}} <small>mm</small></div>
-        <div class="weatherForecastItemInfo">
-          <div class="weatherForecastItemInfoPressure">
-            <img src="/static/pressure.svg"/> {{item.mslp.metric}} <small>hPa</small>
-          </div>
-        </div>
-      </li>
-    </ul>
   </div>
 </template>
 
@@ -167,6 +91,17 @@
               id: 'temp',
               display: false,
             }],
+            xAxes: [{
+              id: 'time',
+              ticks: {
+                display: false,
+              },
+              gridLines: {
+                drawBorder: false,
+                drawTicks: false,
+                tickMarkLength: 0,
+              },
+            }],
           },
           legend: {
             display: false,
@@ -175,6 +110,9 @@
             enabled: false,
           },
           elements: {
+            line: {
+              borderCapStyle: 'round',
+            },
             point: {
               radius: 0,
               hitRadius: 0,
@@ -183,7 +121,7 @@
           },
           layout: {
             padding: {
-              top: 10, // margin for top datalabels
+              top: 20, // margin for top datalabels
             },
           },
           plugins: {
@@ -237,6 +175,9 @@
             }],
             xAxes: [{
               id: 'time',
+              ticks: {
+                display: false,
+              },
               gridLines: {
                 tickMarkLength: 20, // (default 10) extra margin for bottom labels
               },
@@ -254,6 +195,9 @@
             enabled: false,
           },
           elements: {
+            line: {
+              borderCapStyle: 'round',
+            },
             point: {
               radius: 0,
               hitRadius: 0,
@@ -296,6 +240,9 @@
             enabled: false,
           },
           elements: {
+            line: {
+              borderCapStyle: 'round',
+            },
             point: {
               radius: 0,
               hitRadius: 0,
@@ -314,6 +261,7 @@
           this.chartTemp.data.datasets = [{
             fill: false,
             type: 'line',
+            xAxisID: 'time',
             yAxisID: 'temp',
             label: 'Temp',
             data: temps,
@@ -459,60 +407,41 @@
   .chart-container {
     position: relative;
   }
-  .chart-temp,
-  .chart-rain,
+  .chart-temp {
+    height: 200px;
+  }
   .chart-sky-air {
     height: 150px;
   }
-  .weatherForecast .weatherForecastItem {
-    width: 8em;
-    margin: 1em;
-    display: inline-block;
+  .chart-rain {
+    height: 100px;
   }
-  .weatherForecastItemInfo {
-    margin: 0.2em 0;
+  .weatherForecast {
+    padding: 0 8px 0 14px;
+    display: flex;
+    flex-direction: row;
+  }
+  .weatherForecast .weatherForecastItem {
+    display: inline-block;
+    flex-grow: 1;
   }
   .weatherForecastItemInfoTime {
-    font-weight: bold;
+    /*font-weight: bold;*/
   }
   .weatherForecastItemInfoTime,
-  .weatherForecastItemInfoIcon {
+  .weatherForecastItemInfoIcon,
+  .weatherForecastItemInfoWind {
     text-align: center;
   }
   .weatherForecastItemInfoWind {
     vertical-align: middle;
-    width: 4em;
-    height: 4em;
     display: inline-block;
   }
   .weatherForecastItemInfoWind > img {
     width: 100%;
   }
-  .weatherForecastItemInfoCloudiness {
-    line-height: 2em;
-  }
-  .weatherForecastItemInfoCloudiness > img {
-    width: 2em;
-    vertical-align: bottom;
-  }
-  .weatherForecastItemInfoTemperature,
-  .weatherForecastItemInfoHumidity,
-  .weatherForecastItemInfoPressure,
-  .weatherForecastItemInfoUV {
-    line-height: 1.5em;
-  }
-  .weatherForecastItemInfoTemperature > img,
-  .weatherForecastItemInfoHumidity > img,
-  .weatherForecastItemInfoPressure > img,
-  .weatherForecastItemInfoUV > img {
-    height: 1.5em;
-    width: 2em;
-    vertical-align: bottom;
-  }
-  .weatherForecastItemInfoRain {
-    vertical-align: middle;
-    width: 2em;
-    height: 2em;
-    display: inline-block;
+  .weatherForecastItemInfoIcon .wu {
+    width: 30px;
+    height: 30px;
   }
 </style>
